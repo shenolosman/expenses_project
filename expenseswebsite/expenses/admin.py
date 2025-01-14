6ea8c059bdeca83b9aaf55447e76c9c8bcd2039c
Shenol <shenol88@hotmail.com>
2025-01-14 19:56:39 +0100

    modifying admin pages

--- expenseswebsite/expenses/admin.py
+++ expenseswebsite/expenses/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Expense, Category
 
-# Register your models here.
 
-admin.site.register(Expense)
+# Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        # "name",
+        "amount",
+        # "currency",
+        "date",
+        "category",
+        "owner",
+    )
+    search_fields = (
+        # "name",
+        "amount",
+        # "currency",
+        "date",
+        "category",
+        "owner",
+    )
+    list_per_page = 25
+
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
